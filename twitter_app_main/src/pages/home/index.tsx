c6cb9113a0c3f86bc22449071cb5af68aedcb619
songye38 <ddev0308@gmail.com>
2023-12-23 03:26:23 +0900

    Update index.tsx Home 컴포넌트에서 메인 화면 작업

--- twitter_app_main/src/pages/home/index.tsx
+++ twitter_app_main/src/pages/home/index.tsx
@@ -1,3 +1,124 @@
+import {FiImage} from 'react-icons/fi'
+import { Link } from 'react-router-dom';
+import { FaUserCircle } from 'react-icons/fa';
+
+
+export interface PostProps {
+    id : string;
+    email : string;
+    content : string;
+    createdAt : string;
+    uid : string;
+    profileUrl? : string;
+    likeCount? : number;
+    comments? : any;
+}
+
+//더미 데이터
+const posts : PostProps[] = [
+    {
+        id : '1',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '2',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '3',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '4',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '5',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '6',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+    {
+        id : '7',
+        email : 'vsongyev@hanmail.net',
+        content : 'content 1',
+        createdAt : '2023-12-23',
+        uid : '123123',
+    },
+]
+
+
 export default function HomePage(){
-    return <h1>Home Page</h1>
+
+    const handleFileUpload = ()=>{
+
+    }
+
+
+    return (
+        <div className="home">
+            <div className="home__title"></div>
+            <div className="home__tabs">
+                <div className="home__tab home__tab--active">For You</div>
+                <div className="home__tab">Following</div>
+            </div>
+            {/* Post Form */}
+            <form className="post-form">
+                <textarea 
+                className="post-form__textarea" 
+                required 
+                name='content' 
+                id='content'
+                placeholder="What is happening?" />
+            <div className="post-form__submit-area">
+                <label htmlFor='file-input' className='post-form__file'>
+                    <FiImage className='post-form__file-icon'></FiImage>
+                </label>
+                <input 
+                type='file' 
+                name='file-input' 
+                accept='image/*' 
+                onChange={handleFileUpload}
+                className='hidden'
+                />
+                <input type='submit' value='Tweet' className='post-form__submit-btn' />
+            </div>
+            </form>
+            {/* Tweet Posts */}
+            <div className='post'>
+                {posts?.map((post)=>(
+                    <Link to={`/posts/${post?.id}`}>
+                        <div className='post__box-profile'>
+                            <div className='post__flex'>
+                                {post?.profileUrl ? <img src={post?.profileUrl} alt='profile' className='post__box-proflie-img'/> : <FaUserCircle className='post__box-profile-icon'/> }
+                                <div className='post__email'>{post?.email}</div>
+                                <div className='post__createdAt'>{post?.createdAt}</div>
+                            </div>
+                            <div className='post__box-content'>{post?.content}</div>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
