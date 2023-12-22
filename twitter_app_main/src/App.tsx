74ec79bc59098d08cbe7f5ad13ebbd442398777e
songye38 <ddev0308@gmail.com>
2023-12-23 02:59:56 +0900

    Add Layout.tsx Layout 컴포넌트 생성 및 연결

--- twitter_app_main/src/App.tsx
+++ twitter_app_main/src/App.tsx
@@ -1,11 +1,13 @@
-import HomePage from 'pages/home';
+
+import { Layout } from 'components/Layout';
 import './App.css';
-import { Route,Routes,Navigate } from 'react-router-dom';
 import Router from 'components/Router';
 
 function App() {
   return (
-    <Router />
+    <Layout >
+       <Router />
+    </Layout>
   );
 }
 
