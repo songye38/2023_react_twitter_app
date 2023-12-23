df9c25e9d9d6a30356c712c0e15db8c8190e30dd
songye38 <ddev0308@gmail.com>
2023-12-23 11:07:36 +0900

    Update SignupForm.tsx 상태관리를 위해 각 입력값 별로 상태 추가

--- twitter_app_main/src/components/users/SignupForm.tsx
+++ twitter_app_main/src/components/users/SignupForm.tsx
@@ -1,26 +1,36 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignupForm(){
+
+    const [error,setError] = useState<string>("");
+    const [email,setEmail] = useState<string>("");
+    const [password,setpassword] = useState<string>("");
+    const [passwordConfirmation,setPasswordConfirmation] = useState<string>("");
+
     return (
 
         <form className="form form--lg">
             <div className="form__title">회원가입</div>
             <div className="form__block">
                 <label htmlFor="email">이메일</label>
-                <input type='text' name='email' id='email' required />
+                <input type='text' name='email' id='email'  value={email} required />
             </div>
             <div className="form__block">
                 <label htmlFor="password">비밀번호</label>
-                <input type='password' name='password' id='password' required />
+                <input type='password' name='password' id='password' value = {password} required />
             </div>
             <div className="form__block">
                 <label htmlFor="password_confirmation">비밀번호 확인</label>
-                <input type='password' name='password_confirmation' id='password_confirmation' required />
+                <input type='password' name='password_confirmation' id='password_confirmation' value = {passwordConfirmation}required />
             </div>
             {/*만약 에러가 있다면? */}
-            <div className="form__block">
-                <div className="form_error">{"에러 내용"}</div>
-            </div>
+            {error && error?.length >0 && (
+                <div className="form__block">
+                    <div className="form_error">{error}</div>
+                </div>
+            )}
+
             <div className="form__block">
                 계정이 있으신가요?
                 <Link to='/login' className='form__link'>
@@ -28,7 +38,7 @@ export default function SignupForm(){
                 </Link>
             </div>
             <div className="form__block">
-                <button type='submit' className="form__btn-submit">회원가입</button>
+                <button type='submit' className="form__btn-submit" disabled = {error?.length >0}>회원가입</button>
             </div>
         </form>
     );
