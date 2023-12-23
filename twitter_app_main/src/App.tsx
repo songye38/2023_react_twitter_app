6bd461c5af2a895ee3c86ccfc58fcd5f6d7cc4d8
songye38 <ddev0308@gmail.com>
2023-12-23 10:32:18 +0900

    Update App.tsx 현재 사용자 정보 가져오기

--- twitter_app_main/src/App.tsx
+++ twitter_app_main/src/App.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import { Layout } from 'components/Layout';
 import Router from 'components/Router';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -6,7 +7,11 @@ import { app } from 'firebaseApp';
 
 function App() {
   const auth = getAuth(app);
-  console.log(auth);
+  const [isAuthenticated,setAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+    //false면 인증된 사용자가 아니므로 로그인 / 회원가입 페이지로 이동
+  );
+
   return (
     <Layout >
        <Router />
