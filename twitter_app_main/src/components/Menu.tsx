7a1a77d85ac1a22d8ed6b392ae87b90999c5bfbc
songye38 <ddev0308@gmail.com>
2023-12-23 14:53:19 +0900

    Update Menu.tsx signOut기능을 통해 로그아웃 구현

--- twitter_app_main/src/components/Menu.tsx
+++ twitter_app_main/src/components/Menu.tsx
@@ -5,6 +5,9 @@ import {BiUserCircle} from 'react-icons/bi';
 import {MdLogout,MdLogin} from 'react-icons/md'
 import AuthContext from "context/AuthContext";
 import { useContext } from "react";
+import { getAuth,signOut } from "firebase/auth";
+import { app } from "firebaseApp";
+import { toast } from "react-toastify";
 
 export default function MenuList(){
     const {user} = useContext(AuthContext);
@@ -13,7 +16,13 @@ export default function MenuList(){
         <div className="footer__grid">
             <button type='button' onClick={()=>navigate('/')}><BsHouse/> Home</button>
             <button type='button' onClick={()=>navigate('/profile')}><BiUserCircle/> Profile</button>
-            {user===null ? (<button type='button' onClick={()=>navigate('/users/login')}><MdLogin/> Login</button>) : (<button type='button' onClick={()=>navigate('/')}><MdLogout/> Logout</button>)}
+            {user===null 
+            ? (<button type='button' onClick={()=>navigate('/users/login')}><MdLogin/> Login</button>) 
+            : (<button type='button' onClick={async ()=>{
+                const auth = getAuth(app);
+                await signOut(auth);
+                toast.success('로그아웃되었습니다.');
+            }}><MdLogout/> Logout</button>)}
         </div>
     </div>
 }
