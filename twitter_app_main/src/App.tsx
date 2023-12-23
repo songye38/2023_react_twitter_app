ccad59f153f33f834d2ce58db288a8edb89c539f
songye38 <ddev0308@gmail.com>
2023-12-23 14:03:35 +0900

    Update Loader.module.scss 로더 모듈 추가 및 스타일링

--- twitter_app_main/src/App.tsx
+++ twitter_app_main/src/App.tsx
@@ -6,6 +6,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from 'firebaseApp';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from 'components/loader/Loader';
 
 function App() {
   const auth = getAuth(app);
@@ -29,7 +30,7 @@ function App() {
   return (
     <Layout>
       <ToastContainer/>
-      {init ? <Router isAuthenticated={isAuthenticated} /> :"loading"}
+      {init ? <Router isAuthenticated={isAuthenticated} /> :<Loader />}
     </Layout>
   );
 }
